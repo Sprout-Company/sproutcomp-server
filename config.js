94e3f45c84e29a870d95b0594fd9f98c07c8aff1
TheNewestNewbie <thenewestnewbie@gmail.com>
2024-04-05 12:36:30 -0500

    feat (server/config) : added config file

---
+++ config.js
@@ -0,0 +1,9 @@
+//Config General
+
+const config = {
+    DIR: __dirname,
+    SERVER: config.DIR + "/server",
+    HOST: "www.sproutcomp.pro",
+    URL: "https://www.sproutcomp.pro",
+    PORT: process.env.PORT || 3000
+};

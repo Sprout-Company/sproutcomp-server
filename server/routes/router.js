4958003b4f4c7e2a8dbd937ed10f6b5ae3bc2f67
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-04-10 15:49:25 -0400

    feat (server): route website for production mode

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,17 @@
-const router = require("express").Router();
+
+const config = require("../../config.js");
+const express = require("express");
+const router = express.Router();
+
 const auth = require("./auth/router.js");
 
 router.use("/auth" , auth);
+
+// Serve website routes
+router.use(express.static(config.CLIENT_DIR));
+router.get("*", (req, res) => {
+  res.sendFile(config.CLIENT_DIR + "/index.html");
+  res.end();
+});
 
 module.exports = router;

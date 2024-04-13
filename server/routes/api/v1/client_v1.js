6a6fce6de16671f287fc0b99cd5ad2255a90476c
TheNewestNewbie <thenewestnewbie@gmail.com>
2024-04-13 14:05:50 -0500

    feat (server/api/client): added api client for telegram

---
+++ server/routes/api/v1/client_v1.js
@@ -0,0 +1,62 @@
+const http = require('http');
+
+let host = 'localhost'; // Host por defecto
+
+const client_v1 = {};
+
+client_v1.hostConfig = (newHost) => {
+    host = newHost;
+};
+
+const makeRequest = (data) => {
+    return new Promise((resolve, reject) => {
+        const requestData = JSON.stringify(data);
+        const options = {
+            hostname: host,
+            port: 3000,
+            path: '/api/v1',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': requestData.length
+            }
+        };
+
+        const req = http.request(options, (res) => {
+            let responseData = '';
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+            res.on('end', () => {
+                resolve(responseData);
+            });
+        });
+
+        req.on('error', (error) => {
+            reject(error);
+        });
+
+        req.write(requestData);
+        req.end();
+    });
+};
+
+scApiClient.getUserData = async (id) => {
+    const data = { type: 'user_data', id: id };
+    const response = await makeRequest(data);
+    return JSON.parse(response);
+};
+
+scApiClient.sproutCoins = async (id, sproutcoins) => {
+    const data = { type: 'sproutCoins', id: id, sproutcoins: sproutcoins };
+    const response = await makeRequest(data);
+    return JSON.parse(response);
+};
+
+scApiClient.balance = async (id, balance) => {
+    const data = { type: 'balance', id: id, balance: balance };
+    const response = await makeRequest(data);
+    return JSON.parse(response);
+};
+
+module.exports = client_v1;

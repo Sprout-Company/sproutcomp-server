17f530382973f7b92eb9d36dc54b54e423873d7a
TheNewestNewbie <thenewestnewbie@gmail.com>
2024-04-13 08:46:54 -0500

    feat (server/api/balance): added balance change endpoint

--- server/routes/api/v1/v1.js
+++ server/routes/api/v1/v1.js
@@ -1,29 +1,33 @@
 const user_data = require("./user_data.js");
+const sproutCoins = require("./sproutCoins.js");
+const balance = require("./balance.js");
 
 const v1 = async (req, res, next) => {
-    try {
-        // Acceder a los datos enviados en el cuerpo de la solicitud POST
-        const data = req.body;
-        // Obtener la IP del cliente
-        const clientIP = req.ip;
+  try {
+    // Acceder a los datos enviados en el cuerpo de la solicitud POST
+    const data = req.body;
+    // Obtener la IP del cliente
+    const clientIP = req.ip;
 
-        if(data.type) switch(data.type){
-            case "user_data":
-                user_data(res , data);
-                break;
-            case "sproutCoins":
-                sproutCoins(res , data);
-                break;
-            default:
-                res.status(200).json({status: 'ERROR' , message: 'NO_DATATYPE_FOUND' });
-        }
-
-        res.status(200).json({status: 'ERROR' , message: 'NO_DATATYPE' });
-    } catch (error) {
-        // Manejar cualquier error que ocurra
-        console.error('Error:', error);
-        res.status(500).json({status: 'ERROR' , message: 'NO_VALID_DATA' });
-    }
+    if (data.type) {
+      switch (data.type) {
+        case "user_data":
+          user_data(res, data);
+          break;
+        case "sproutCoins":
+          sproutCoins(res, data);
+          break;
+        case "balance":
+          balance(res, data);
+        default:
+          res.status(200).json({ status: 'ERROR', message: 'NO_DATATYPE_FOUND' });
+      }
+    } else res.status(200).json({ status: 'ERROR', message: 'NO_DATATYPE' });
+  } catch (error) {
+    // Manejar cualquier error que ocurra
+    console.error('Error:', error);
+    res.status(500).json({ status: 'ERROR', message: 'NO_VALID_DATA' });
+  }
 }
 
 module.exports = v1;

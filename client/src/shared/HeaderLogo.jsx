6082cc6b35cdd428ec008257681aee5d1104fd99
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-04-15 03:45:14 -0400

    fix (ui): fix navigate hook name

--- client/src/shared/HeaderLogo.jsx
+++ client/src/shared/HeaderLogo.jsx
@@ -1,13 +1,13 @@
 
-import { useNavigation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { FaDice } from 'react-icons/fa'
 
 export default function HeaderLogo ({ className, redirect }) {
-  const navigate = useNavigation();
+  const navigate = useNavigate(); 
   
   return (
     <div 
-      onClick={() => redirect && navigate('/')}
+      onClick={() => {redirect && navigate('/')}}
       className={`flex items-center font-bold text-white ${className}`}
     >
       <h1> Sprout<span className='text-red-900'>C</span>omp </h1>

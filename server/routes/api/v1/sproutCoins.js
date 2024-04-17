1c9c0dba11e43d43655350f1a08a8ecb00af5378
TheNewestNewbie <thenewestnewbie@gmail.com>
2024-04-17 12:40:08 -0500

    fix (server/api)L fixed database objectid search

--- server/routes/api/v1/sproutCoins.js
+++ server/routes/api/v1/sproutCoins.js
@@ -1,4 +1,5 @@
 const config = require("../../../../config.js");
+const mongoose = require("mongoose");
 const User = require(config.DB_DIR + "/models/User.js");
 const Wallet = require(config.DB_DIR + "/models/Wallet.js");
 
@@ -7,7 +8,7 @@ module.exports = async (res, data) => {
   if (!data.sproutcoins) return res.status(200).json({ status: 'ERROR', message: 'DATA_NOT_FOUND' });
 
   try {
-    const user = await User.findOne({ $or: [{ telegram_id: data.id }, { _id: data.id }] });
+    const user = await User.findOne({ $or: [{ telegram_id: data.id }, { _id: mongoose.Types.ObjectId(data.id) }] });
     if (!user) return res.status(200).json({ status: 'ERROR', message: 'USER_NOT_FOUND' });
 
     const wallet = await Wallet.findOne({ userId: user._id });
--- server/routes/api/v1/udata.js
+++ server/routes/api/v1/udata.js
@@ -1,4 +1,5 @@
 const config = require("../../../../config.js");
+const mongoose = require("mongoose");
 const User = require(config.DB_DIR + "/models/User.js");
 const Wallet = require(config.DB_DIR + "/models/Wallet.js");
 
@@ -6,7 +7,7 @@ module.exports = async (res, data) => {
   if (!data.id) return res.status(200).json({ status: 'ERROR', message: 'ID_NOT_FOUND' });
 
   try {
-    const user = await User.findOne({ $or: [{ telegram_id: data.id }, { _id: data.id }] });
+    const user = await User.findOne({ $or: [{ telegram_id: data.id }, { _id: mongoose.Types.ObjectId(data.id) }] });
     if (!user) return res.status(200).json({ status: 'ERROR', message: 'USER_NOT_FOUND' });
 
     const wallet = await Wallet.findOne({ userId: user._id });

f82a3533133dfdaa808f0c66aa6cd94a83f571dc
TheNewestNewbie <thenewestnewbie@gmail.com>
2024-04-22 13:56:10 -0500

    feat (server/payments): added paypal callback route

--- server/routes/payments/paypal/callback.js
+++ server/routes/payments/paypal/callback.js
@@ -8,30 +8,32 @@ const callback = async (req, res) => {
 
     if (data) {
         try {
-            if (data.status == "success") {}
-            // Buscar la transacción por su ID en la base de datos
-            const trans = await Transactions.findOne({ transactionId: data.id });
+            if (data.status == "completed") {
+                // Buscar la transacción por su ID en la base de datos
+                const trans = await Transactions.findOne({ transactionId: data.id });
 
-            if (!trans) {
-                return res.status(404).json({ status: 'ERROR', message: 'TRANSACTION_NOT_FOUND' });
+                if (!trans) {
+                    return res.status(404).json({ status: 'ERROR', message: 'TRANSACTION_NOT_FOUND' });
+                }
+
+                // Actualizar el estado de la transacción
+                trans.status = data.status;
+                if (data.status == "completed" && trans.processed == false) {
+                    trans.processed = true;
+                    const wallet = await Wallet.findOne({ userId: trans.userId });
+                    if (!wallet) return res.status(404).json({ status: 'ERROR', message: 'WALLET_NOT_FOUND' });
+                    wallet.sprout_coins += trans.sproutcoins;
+                    await wallet.save();
+                }
+                await trans.save();
+
+                res.status(200).json({ status: 'SUCCESS', message: 'PAYMENT_DONE' });
             }
-
-            // Actualizar el estado de la transacción
-            trans.status = data.status;
-            if (data.status == "completed" && trans.processed == false) {
-                trans.processed = true;
-                const wallet = await Wallet.findOne({ userId: trans.userId });
-                if (!wallet) return res.status(404).json({ status: 'ERROR', message: 'WALLET_NOT_FOUND' });
-                wallet.sprout_coins += trans.sproutcoins;
-                await wallet.save();
-            }
-            await trans.save();
-
-            res.status(200).json({ status: 'SUCCESS', message: 'PAYMENT_DONE' });
         } catch (error) {
             console.error('Error al actualizar la transacción:', error);
             res.status(500).json({ status: 'ERROR', message: 'INTERNAL_SERVER_ERROR' });
         }
+
     } else {
         return res.status(404).json({ status: "ERROR", message: "WRONG_DATA" });
     }
--- server/routes/payments/paypal/router.js
+++ server/routes/payments/paypal/router.js
@@ -4,9 +4,10 @@ const User = require(config.DB_DIR + "/models/User.js");
 const Wallet = require(config.DB_DIR + "/models/Wallet.js");
 
 const buysc = require("./buysc.js");
+const callback = require("./callback.js");
 
 router.post('/buysc' , buysc);
 
-router.
+router.post('/success' , callback);
 
 module.exports = router;

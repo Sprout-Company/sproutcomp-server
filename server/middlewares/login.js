69169cef46229e8bb26e3e387d005b3f7941f4fa
TheNewestNewbie <thenewestnewbie@gmail.com>
2024-05-04 02:04:07 -0500

    chore (package.json): changed bcrypt module to bcryptjs

--- server/middlewares/login.js
+++ server/middlewares/login.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const config = require("../../config.js");
 const User = require(config.DB_DIR + "/models/User.js");
 
